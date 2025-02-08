b9de8e7c0b18a95486ec6944fdeadca31606e3cb
deibjaighceq <deibjaighcequinot@gmail.com>
2025-02-08 09:18:57 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 class Server {
     async startServer() {
         const colors = require('colors');
-        const path = require('path');
+        const path = require('path'); 
         const express = require('express');
-
         const {
             displayHeader,
             accinfo,
             login,
+            ptcAds,
             spin
         } = require('./src/index');
-        const port = process.env.PORT || 8080;
-
-        require('dotenv').config({
-            path: path.resolve(__dirname, './.env')
-        });
+        require('dotenv').config();
         const username = process.env.USERNAME;
         const password = process.env.PASSWORD;
+        const port = process.env.PORT || 3000;
         colors.setTheme({
             silly: 'rainbow',
             input: 'grey',
@@ -31,21 +28,21 @@ class Server {
         });
 
         const app = express();
-
+        
         function delay(ms) {
             return new Promise(resolve => setTimeout(resolve, ms));
         }
-        
+
         async function runTasks() {
             try {
                 let cookies = await login(username, password);
                 let resp = await spin(cookies);
-                if (typeof resp === 'undefined') throw new Error("Please wait until the timer runs out and try again later.");
-                console.log(colors.info.bold("[ SCRIPT ] ")+colors.info(resp.message))
+                if (typeof resp.message === 'undefined') throw new Error("Please wait until the timer runs out and try again later.");
+                console.log(colors.info.bold("[ SPIN ] ")+colors.info(resp.message))
                 await accinfo(cookies, true);
             }
             catch(e) {
-                console.log(colors.warn.bold("[ SCRIPT ] ")+colors.error(e.error))
+                console.log(colors.warn.bold("[ SCRIPT ] ")+colors.error(e.toString()))
                 console.log(colors.warn.bold("[ SCRIPT ] ")+colors.warn("Reconnecting..."))
                 await delay(60000);
                 await runTasks();
@@ -58,17 +55,27 @@ class Server {
                 await delay(600000);
             }
         }
+        
+        async function runPtcViewer() {
+            while (true) {
+                let cookies = await login(username, password);
+                let res1 = await ptcAds(cookies);
+                console.log(colors.info.bold("[ PTC ADS ]")+colors.info(res1))
+                await accinfo(cookies, true)
+            }
+        }
 
         app.get('/', (req, res) => {
             res.send('SERVER FOR BITBON FAUCET - AUTO FARM SCRIPT\nMADE\nBY\nHackMeSenpai(HMS)')
         });
         
-        app.listen(port, async() => {;
+        app.listen(port, async() => {
             displayHeader();
             console.log(colors.verbose.bold("[ SERVER ]")+colors.info(` Server port ${port} exposed!`));
             let cookies = await login(username, password);
             await accinfo(cookies);
             runEveryTenMinutes();
+            runPtcViewer();
         });
     }
 }
